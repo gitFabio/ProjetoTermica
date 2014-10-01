1959342006a3a4a798754266b62a808cb6e5bcf0
gitFabio <dornel.fabio2@gmail.com>
2014-10-01 20:05:42 -0300

    Esboco do projeto a ser desenvolvido

---
+++ ProjetoTermica.ino
@@ -0,0 +1,26 @@
+/*
+Desenvolver uma termica que ser possivel atraves de botoes selecionar
+a temperatura maxima a ser atingida pela agua, e quando a temperatura
+maxima for atingida devera ter um aviso sonoro e um visual no
+monitor lcd (16,2) informando que a agua chegou na temperatura
+selecionada pelo usuario, para encerrar o aviso sera necessario o usuario
+pressionar os 2 botoes, e caso a termica continue ligado na luz ela ficara
+monitorando e controlando a temperatura da agua.
+
+Materiais necessarios:
+
+Display LCD 16,2;
+3 botoes;
+1 buzzer;
+1 potenciometro
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
