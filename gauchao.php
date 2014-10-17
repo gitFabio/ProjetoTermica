0b95f85c0b8c29e52664f2c7ee5f7e48ba25ca25
root <root@debianAcer>
2014-10-16 22:50:29 -0300

    Feito um exemplo para mandar dados do php para o arquino

--- gauchao.php
+++ gauchao.php
@@ -1,13 +1,17 @@
 
 <?php
-
+/*
 	$hDesp = $_POST["hora"];
 	$mDesp = $_POST["minutos"];
-	$temperatura = $_POST["temp"];
+	$temperatura = $_POST["temp"];*/
+	$hDesp = "8";
+	$mDesp = "0";
+	$temperatura = "90";
 	$hAtual = date("h");
 	$mAtual = date("i");
 	$sAtual = date("s");
 
+	$enviar = "#".$hAtual."#"."%".$mAtual."%"."*".$sAtual."*"."@".$temperatura."@"."!".$hDesp."!"."=".$mDesp."=";
 
 	echo nl2br("Hora atual " . $hAtual . "h " . $mAtual . "m " . $sAtual ."s");
 
@@ -16,17 +20,20 @@
 	echo nl2br("\nHora [". $hDesp . "]\n Minuto [". $mDesp . "]");
 	echo nl2br("\nTemperatura " . $temperatura . "C");
 
-	$port = fopen("/dev/ttyACM0", "w");
-
+try{
+	$port = '/dev/ttyUSB0';
+	$con = fopen($port, 'r+');
 
 	echo nl2br("\n\n===============");
 	echo nl2br("\n\tTeste do arduino");
 
+	fwrite($con, $enviar);
+	echo nl2br("\nLigou sucesso\n");
 	
-	if(fwrite($port, "a")){
-		echo nl2br("\nLigou sucesso");
-	}else{
-		echo nl2br("\nNao ligou");
-	}
+}catch(Exeption $e){
+	echo nl2br("\nNao ligou\n");
+	echo nl2br("Erro ", $e.getMessage);
+}
+
 
 ?>
